40e18fec332d9c5a6bb07e9cbc2486f950212904
Francisco Gómez Carrasco <flikxxi@gmail.com>
2009-04-10 08:13:20 +0000

    - created JavaProperty

---
+++ JavaProperty/src/javaproperty/JavaProperty.java
@@ -0,0 +1,57 @@
+/*
+ *  JavaProperty.java
+ *
+ *  Copyright (C) 2009  Francisco Gómez Carrasco
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Report bugs or new features to: flikxxi@gmail.com
+ *
+ */
+package javaproperty;
+
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ *
+ * @author franci
+ */
+public class JavaProperty
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        if (args.length > 0)
+        {
+            for (String item : args)
+            {
+                System.out.println(item + "=" + System.getProperty(item.toString()));
+            }
+        }
+        else
+        {
+            Properties prop = System.getProperties();
+            Set<Object> set = prop.keySet();
+            for (Object item : set)
+            {
+                String key = item.toString();
+                System.out.println(key + "=" + prop.getProperty(key.toString()));
+            }
+        }
+    }
+}
